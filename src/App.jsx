66b7becf6d38401ffbb2747768b5820ab45fefcc
bakNa2t <ok2.lock@gmail.com>
2024-07-22 12:19:52 +0300

    Added GlobalStyles to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-  return <h1>Welcome to Type 2 Note</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Welcome to Type 2 Note</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,93 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+:root {
+    &, &.light-mode {
+        --color-blue: #3a7bfd;
+        --color-blue-dark: #0070f3;
+
+        --color-blue-zola-600: #393a4c;
+        --color-blue-zola-700: #25273c;
+        --color-blue-light-200: #e4e5f1;
+        --color-blue-light-300: #c2c2c2;
+        --color-blue-light-700: #777a92;
+    }
+
+    &.dark-mode {
+        --color-blue-dark: #0070f3;
+        --color-blue-zola-600: #393a4c;
+        --color-blue-zola-700: #25273c;
+        --color-blue-light-200: #e4e5f1;
+        --color-blue-light-300: #c2c2c2;
+        --color-blue-light-700: #777a92;
+
+    }
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+
+  /* Animations for dark mode */
+  transition: background-color 0.3s, border 0.3s;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 100vh;
+    line-height: 1.5;
+    font-size: 1.6rem;
+
+    font-family: 'Fira Code', monospace;
+    background-color: var(--color-blue-dark);
+    color: var(--color-blue-light-200);
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+*:disabled {
+  cursor: not-allowed;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+`;
+
+export default GlobalStyles;
