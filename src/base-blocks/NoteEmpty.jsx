99d82cc189e79eb3fe9f70530e7c15a4e146385f
bakNa2t <ok2.lock@gmail.com>
2024-07-25 19:00:00 +0300

    Added NoteEmpty

---
+++ src/base-blocks/NoteEmpty.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NoteEmpty = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  border-bottom: 1px solid var(--color-blue-light-300);
+`;
+
+export default NoteEmpty;
