617f649e6d5a54fab311991078f80ef5fc8d94d6
bakNa2t <ok2.lock@gmail.com>
2024-07-26 11:24:06 +0300

    Added NoteEmpty or NoteItem if filteredNotes length is 0 or not

--- src/ui-blocks/NoteField.jsx
+++ src/ui-blocks/NoteField.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 import NoteItem from "../base-blocks/NoteItem";
+import NoteEmpty from "../base-blocks/NoteEmpty";
+
+import { selectFilteredNotes } from "../base-blocks/noteSlice";
 
 const StyledNoteField = styled.ul`
   display: flex;
@@ -11,9 +15,16 @@ const StyledNoteField = styled.ul`
 `;
 
 function NoteField() {
+  const filteredNotes = useSelector(selectFilteredNotes);
+  console.log(filteredNotes);
+
   return (
     <StyledNoteField>
-      <NoteItem />
+      {filteredNotes.length === 0 ? (
+        <NoteEmpty>Here will be your notes</NoteEmpty>
+      ) : (
+        <NoteItem />
+      )}
     </StyledNoteField>
   );
 }
