d55c9c1183198b94ff4bf0391873cb986321e47a
bakNa2t <ok2.lock@gmail.com>
2024-07-31 11:38:54 +0300

    Added NoteFilterWrapper

---
+++ src/ui-blocks/NoteFilterWrapper.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NoteFilterWrapper = styled.div`
+  width: 100%;
+  margin-top: 1rem;
+  padding: 1rem 0;
+  border-radius: 1rem;
+  background-color: var(--color-ebony-800);
+`;
+
+export default NoteFilterWrapper;
