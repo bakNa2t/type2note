7d4ae3bff836cc28b7b33623330d173350336dff
bakNa2t <ok2.lock@gmail.com>
2024-08-01 14:32:22 +0300

    Updated Input's ::selection

--- src/ui-blocks/Input.jsx
+++ src/ui-blocks/Input.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const StyledInput = styled.input`
   width: 100%;
-  padding: 1rem 1rem 1rem 2rem;
+  padding: 1rem 4.4rem 1rem 2rem;
   border: none;
   outline: none;
   background-color: transparent;
@@ -14,6 +14,11 @@ const StyledInput = styled.input`
     font-style: italic;
     color: var(--color-ebony-400);
     filter: opacity(0.8);
+  }
+
+  &::selection {
+    color: var(--color-spring-green-800);
+    background-color: var(--color-spring-green-200);
   }
 `;
 
