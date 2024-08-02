6d84f1bb7db686015cc2d4e268ecd6ec0548cfb7
bakNa2t <ok2.lock@gmail.com>
2024-08-02 12:53:36 +0300

    Added NoteCounterContext

---
+++ src/context/NoteCounterContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext } from "react";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+
+const NoteCounterContext = createContext();
+
+function NoteCounterProvider({ children }) {
+  NoteCounterProvider.propTypes = {
+    children: PropTypes.node,
+  };
+
+  const activeNotes = useSelector(
+    (state) => state.note.notes.filter((note) => note.completed !== true).length
+  );
+
+  const completedNotes = useSelector(
+    (state) => state.note.notes.filter((note) => note.completed === true).length
+  );
+
+  return (
+    <NoteCounterContext.Provider value={{ activeNotes, completedNotes }}>
+      {children}
+    </NoteCounterContext.Provider>
+  );
+}
+
+function useNoteContext() {
+  const context = useContext(NoteCounterContext);
+
+  if (context === undefined)
+    throw new Error("NoteCounterContext was used outside NoteCounterProvider");
+
+  return context;
+}
+
+export { NoteCounterProvider, useNoteContext };
