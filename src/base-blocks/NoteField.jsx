a4a85117882acfb3544f34c03e8ccfe042a2834e
bakNa2t <ok2.lock@gmail.com>
2024-08-12 13:58:48 +0300

    Moved NoteEmpty and NoteField

---
+++ src/base-blocks/NoteField.jsx
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+import NoteItem from "./NoteItem";
+import NoteEmpty from "../ui-blocks/NoteEmpty";
+
+import { selectFilteredNotes } from "./noteSlice";
+
+const StyledNoteField = styled.ul`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-ebony-800);
+  border-radius: 1rem 1rem 0 0;
+  padding: 1rem;
+`;
+
+function NoteField() {
+  const filteredNotes = useSelector(selectFilteredNotes);
+  const toFiltered = useSelector((state) => state.note.filter);
+
+  return (
+    <StyledNoteField>
+      {filteredNotes.length === 0 ? (
+        toFiltered === "active" ? (
+          <NoteEmpty>Active notes list is empty</NoteEmpty>
+        ) : toFiltered === "completed" ? (
+          <NoteEmpty>Completed notes list is empty</NoteEmpty>
+        ) : (
+          <NoteEmpty>Note list is empty now</NoteEmpty>
+        )
+      ) : (
+        filteredNotes.map((note) => <NoteItem note={note} key={note.id} />)
+      )}
+    </StyledNoteField>
+  );
+}
+
+export default NoteField;
