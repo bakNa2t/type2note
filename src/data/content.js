903688efecb99b5d0c68a72ad0f45c8f80faec35
bakNa2t <ok2.lock@gmail.com>
2024-08-12 12:23:34 +0300

    Added content obj to toggle en/ru lang

---
+++ src/data/content.js
@@ -0,0 +1,10 @@
+export const content = {
+  en: {
+    appTitle: "Get started to store notes",
+    placeholder: "Write your note here...",
+  },
+  ru: {
+    appTitle: "Пора вести заметки",
+    placeholder: "Напишите заметку...",
+  },
+};
