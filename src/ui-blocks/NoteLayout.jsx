9fe89f1b19016965eda5344e249f4c633ac0d4d0
bakNa2t <ok2.lock@gmail.com>
2024-08-21 14:07:51 +0300

    Replaced Heading to ModalHeading

--- src/ui-blocks/NoteLayout.jsx
+++ src/ui-blocks/NoteLayout.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 import Wrapper from "./Wrapper";
-import Heading from "./Heading";
 import InputField from "../base-blocks/InputField";
 import NoteField from "../base-blocks/NoteField";
 import NoteFooterMenu from "./NoteFooterMenu";
 import NoteFilterWrapper from "./NoteFilterWrapper";
+import ModalHeading from "./ModalHeading";
 
 import { useResizeScreen } from "../hooks/useResizeScreen";
+import { useNoteLang } from "../context/NoteLangContext";
+import { contentData } from "../data/content";
 
 const StyledNoteLayout = styled.div`
   flex-grow: 1;
@@ -22,10 +24,13 @@ const StyledNoteLayout = styled.div`
 function NoteLayout() {
   const isMobileSize = useResizeScreen();
 
+  const lang = useNoteLang();
+  const { appTitle } = lang.lang === "en" ? contentData.en : contentData.ru;
+
   return (
     <StyledNoteLayout>
       <Wrapper>
-        <Heading />
+        <ModalHeading as={"h1"}>{appTitle}</ModalHeading>
         <InputField />
         <NoteField />
         <NoteFooterMenu isMobileSize={isMobileSize} />
